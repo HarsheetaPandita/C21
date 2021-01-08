6d99d062249e87d8917061c92d47d14ec095d38d
HarsheetaPandita <75735861+HarsheetaPandita@users.noreply.github.com>
2021-01-08 18:51:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,65 @@
+var fixedRect, movingRect;
+var car, wall;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  car  = createSprite(200,100,50,50);
+  car.shapeColor = "purple";
+  car .velocityX = 5;
+
+  wall = createSprite(900,100,20,150);
+  wall.shapeColor = "orange";
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
+}
+
+function draw() {
+  background(0,0,0);
+
+  if(isTouching(car,wall))
+  {
+   car.shapeColor = "orange";
+  }
+  
+  if(bounceOff(car,wall))
+  {
+    car.velocityX = -5;
+  }
+  drawSprites();
+}
+
+function isTouching(object1,object2)
+{
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object2.width/2 
+    && object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object1.y < object2.height/2 + object1.height/2){
+    return true;
+ }
+ else
+ {
+   return false;
+ }
+}
+
+function bounceOff(object1,object2)
+{
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object2.width/2) {
+      object1.velocityX = object1.velocityX * (-1);
+      object2.velocityX = object2.velocityX * (-1);
+ }
+ if (object1.y - object2.y < object2.height/2 + object1.height/2
+  && object2.y - object1.y < object2.height/2 + object1.height/2){
+    object1.velocityY = object1.velocityY * (-1);
+    object2.velocityY = object2.velocityY * (-1);
+ }
+}
